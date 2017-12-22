ac8d1bd806f14e36015613085d272db6e6515286
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2017-12-22 16:48:09 +0100

    Hoofdstuk 5

--- src/main/java/be/vdab/entities/Rekening.java
+++ src/main/java/be/vdab/entities/Rekening.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 public class Rekening {
 	private BigDecimal saldo = BigDecimal.ZERO;
 	public void storten(BigDecimal bedrag) {
+		if(bedrag.compareTo(BigDecimal.ZERO) <=0) {
+			throw new IllegalArgumentException("Bedrag moet positief zijn");
+		}
 		saldo = saldo.add(bedrag);
 	}
 	
--- src/main/java/be/vdab/valueobjects/Jaar.java
+++ src/main/java/be/vdab/valueobjects/Jaar.java
@@ -8,7 +8,20 @@ public class Jaar {
 	}
 	
 	public boolean isSchrikkeljaar() {
-		return jaar % 4 == 0 && jaar % 100 == 0 || jaar % 400 == 0;
+		return jaar % 4 == 0 && jaar % 100 != 0 || jaar % 400 == 0;
 	}
 	
+	@Override
+	public boolean equals(Object object) {
+		if(!(object instanceof Jaar)) {
+			return false;
+		}
+		Jaar anderJaar = (Jaar) object;
+		return jaar == anderJaar.jaar;
+	}
+	
+	@Override
+	public int hashCode() {
+		return jaar;
+	}
 }
